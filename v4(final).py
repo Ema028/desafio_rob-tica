8713ed8cd5f51b9b829dd0f7a5f795a76dad5fe3
Emanuelle Natal <156372524+Ema028@users.noreply.github.com>
2025-04-23 09:03:36 -0300

    Update v4(final).py

--- v4(final).py
+++ v4(final).py
@@ -28,7 +28,7 @@ color_sensor = ColorSensor(INPUT_1)
 ultra_frente = UltrasonicSensor(INPUT_2)
 ultra_esquerda = UltrasonicSensor(INPUT_3)
 ultra_direita = UltrasonicSensor(INPUT_4)
-gyro_sensor_in5 = GyroSensor(INPUT_5)
+gyro = GyroSensor(INPUT_5)
 
 motorC = LargeMotor(OUTPUT_C) # Magnet
 
@@ -37,73 +37,35 @@ def main():
     tempo = time.time()
     
     while True:
-        if (color_sensor.color == 5): #código para vermelho no ev3
+        check_borda(tempo)
+        
+        if (ultra_frente.distance_centimeters < 30):
+            steering_drive.on(steering = 0, speed = 70)
             tempo = time.time()
-            tank_drive.off()
-            tank_drive.on(left_speed = -100, right_speed = -100)
-            time.sleep(0.5)
-            tank_drive.off()
-            tank_drive.on(left_speed = 70, right_speed = -70)
-            time.sleep(0.5)
-            tank_drive.off()
-            if (color_sensor.color == 5): #se continuar na borda recua mais
-                tank_drive.on(left_speed = -100, right_speed = -100)
-                time.sleep(0.5)
-                tank_drive.off()
-                continue
-            
-        #distância do obstáculo detectado em cada sensor
-        dist_frente = ultra_frente.distance_centimeters
-        dist_esquerda = ultra_esquerda.distance_centimeters
-        dist_direita = ultra_direita.distance_centimeters
-        
-        direcao, found = seguir_obstaculo(dist_frente, dist_esquerda, dist_direita)
-        
-        if is_preso(dist_frente, dist_esquerda, dist_direita):
-            tank_drive.on(left_speed = 70, right_speed = 70)
-            time.sleep(0.5)
-            continue
-        
-        if found:
+        elif (ultra_esquerda.distance_centimeters < 30):
+            steering_drive.on(steering = -50, speed = 70)
+            check_borda(tempo)
             tempo = time.time()
-        
-        if (direcao == 'front'):
-            tank_drive.on(left_speed = 100, right_speed = 100)
-        elif (direcao == 'left'):
-            tank_drive.on(left_speed = -70, right_speed = 70)
+        elif (ultra_direita.distance_centimeters < 30):
+            steering_drive.on(steering = 50, speed = 70)
+            check_borda(tempo)
+            tempo = time.time()
         else:
-            tank_drive.on(left_speed = 70, right_speed = -70)
+            steering_drive.on(steering = 0, speed = 70)
             
         time_now = time.time()
         
         if ((time_now - tempo) > 6):
-            tank_drive.on(left_speed = 70, right_speed = -70)
-            time.sleep(0.5)
-            tank_drive.off()
-            tank_drive.on(left_speed = 70, right_speed = 70)
+            steering_drive.on_for_seconds(steering = 0, speed = -100, seconds = 1)
+            steering_drive.on_for_seconds(steering = 100, speed = 70, seconds = 1)
+            check_borda(tempo)
             tempo = time.time()
         
-#vai em direção ao obstáculo mais perto
-def seguir_obstaculo(a, b, c):
-    direcoes = {'front': a,
-        'left': b,
-        'right': c}
-    
-    if all(distancias > 30 for distancias in direcoes.values()):
-        return 'front', False
-        
-    alvo = min(direcoes, key = direcoes.get)
-    return alvo, True
-    
-#verificar se o robô ficou preso entre obstáculos mt próximos
-def is_preso(a, b, c):
-    distancias = [a, b, c]
-    count = 0
-    for distancia in distancias:
-        if (distancia < 20):
-            count += 1
-            if (count == 2):
-                return True
-    return False
+def check_borda(tempo):
+    if (color_sensor.color == 5): #código para vermelho no ev3
+        steering_drive.off()
+        tempo = time.time()
+        steering_drive.on_for_seconds(steering = 0, speed = -100, seconds = 1)
+        steering_drive.on_for_seconds(steering = 100, speed = 70, seconds = 1)
         
 main()
